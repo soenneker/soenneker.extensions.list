f37136616b1a4c94e7de0ef594b03126fc219791
Jake Soenneker <jake@soenneker.com>
2023-02-16 20:34:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.List.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.List/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.list/main.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.list/actions/workflows/main.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.List.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.List/)
+
+# Soenneker.Extensions.List
+### A collection of helpful list extension methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.List
+```
---
+++ src/ListExtension.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Soenneker.Extensions.Enumerable;
+using Soenneker.Utils.Random;
+
+namespace Soenneker.Extensions.List;
+
+/// <summary>
+/// A collection of helpful list extension methods
+/// </summary>
+public static class ListExtension
+{
+    /// <summary>
+    /// Finds a particular item in the list using a predicate, and replaces it
+    /// </summary>
+    public static void Replace<T>(this List<T> list, Predicate<T> oldItemSelector, T newItem)
+    {
+        //TODO: check for different situations here and throw exception
+        //if list contains multiple items that match the predicate
+        //or check for nullability of list and etc ...
+
+        if (list.None())
+            return;
+
+        int oldItemIndex = list.FindIndex(oldItemSelector);
+
+        if (oldItemIndex != -1)
+            list[oldItemIndex] = newItem;
+    }
+
+    /// <summary>
+    /// Removes a particular item in the list using a predicate
+    /// </summary>
+    public static void Remove<T>(this List<T> list, Predicate<T> oldItemSelector)
+    {
+        //TODO: check for different situations here and throw exception
+        //if list contains multiple items that match the predicate
+        //or check for nullability of list and etc ...
+
+        if (list.None())
+            return;
+
+        int oldItemIndex = list.FindIndex(oldItemSelector);
+
+        if (oldItemIndex != -1)
+            list.RemoveAt(oldItemIndex);
+    }
+
+    /// <summary>
+    /// Simple foreach over toRemove list, removing each from the target
+    /// </summary>
+    public static void RemoveFromList<T>(this IList<T> list, IList<T> toRemove)
+    {
+        if (list.None())
+            return;
+
+        foreach (T item in toRemove)
+        {
+            list.Remove(item);
+        }
+    }
+
+    /// <summary>
+    /// Shuffles the list using a random number. Not cryptographically strong, but thread safe.
+    /// </summary>
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+
+        while (n > 1)
+        {
+            int k = RandomUtil.Next(n--);
+            (list[n], list[k]) = (list[k], list[n]);
+        }
+    }
+
+    /// <summary>
+    /// Shuffles the list using a cryptographically strong number.
+    /// </summary>
+    public static void SecureShuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+
+        while (n > 1)
+        {
+            int k = RandomNumberGenerator.GetInt32(n--);
+            (list[n], list[k]) = (list[k], list[n]);
+        }
+    }
+}
